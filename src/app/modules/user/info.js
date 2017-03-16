82c90c79b6bd5f1adbd7dbaaa756d92c0169fd29
Jjinbao <jinbaow@yeah.net>
2017-03-16 23:06:46 +0800

    修改，用px来作为尺寸单位，增加密码修改页面

--- src/app/modules/user/info.js
+++ src/app/modules/user/info.js
@@ -1,32 +1,32 @@
 'use strict'
 
 angular.module('swalk.userinfo', [])
-    .controller('userInfo', ['$scope','userService','$state','$ionicViewSwitcher','$ionicActionSheet',function ($scope,userService,$state,$ionicViewSwitcher,$ionicActionSheet) {
-        $scope.$on('$ionicView.afterEnter',function(){
+    .controller('userInfo', ['$scope', 'userService', '$state', '$ionicViewSwitcher', '$ionicActionSheet', function ($scope, userService, $state, $ionicViewSwitcher, $ionicActionSheet) {
+        $scope.$on('$ionicView.afterEnter', function () {
             //获取用户信息
             /*userService.getUserInfo({userId:userService.userMess.userId}).then(function(data){
-                if(data.list.errcode===10000){
-                    $scope.userInfo=data.list.data;
-                }else{
-                    $scope.alertTab(data.list.message);
-                }
-            })*/
+             if(data.list.errcode===10000){
+             $scope.userInfo=data.list.data;
+             }else{
+             $scope.alertTab(data.list.message);
+             }
+             })*/
         })
 
-        $scope.changeUserInfo=function(){
-            if(!$scope.userInfo){
+        $scope.changeUserInfo = function () {
+            if (!$scope.userInfo) {
                 return;
             }
             //如果是安卓手机，跳到原生界面
-            if(deviceId=='1'){
+            if (deviceId == '1') {
                 connectWebViewJavascriptBridge(function (bridge) {
                     //回app
                     bridge.callHandler('updateUserInfo', $scope.userInfo, function (response) {
 
                     })
                 });
-            }else{
-                $state.go('setuserinfo',{obj:$scope.userInfo});
+            } else {
+                $state.go('setuserinfo', {obj: $scope.userInfo});
                 $ionicViewSwitcher.nextDirection('forward');
             }
 
@@ -39,33 +39,33 @@ angular.module('swalk.userinfo', [])
         });
 
         //安卓回来之后重新更新数据
-        function androidUpdate(){
-            userService.getUserInfo({userId:userService.userMess.userId}).then(function(data){
-                if(data.list.errcode===10000){
-                    $scope.userInfo=data.list.data;
-                    userService.userMess.birthday=$scope.userInfo.birthday;
-                    userService.userMess.address=$scope.userInfo.address;
-                    userService.userMess.email=$scope.userInfo.email;
-                    userService.userMess.name=$scope.userInfo.name;
-                    userService.userMess.portrait=$scope.userInfo.portrait;
-                    userService.userMess.realName=$scope.userInfo.realName;
-                }else{
+        function androidUpdate() {
+            userService.getUserInfo({userId: userService.userMess.userId}).then(function (data) {
+                if (data.list.errcode === 10000) {
+                    $scope.userInfo = data.list.data;
+                    userService.userMess.birthday = $scope.userInfo.birthday;
+                    userService.userMess.address = $scope.userInfo.address;
+                    userService.userMess.email = $scope.userInfo.email;
+                    userService.userMess.name = $scope.userInfo.name;
+                    userService.userMess.portrait = $scope.userInfo.portrait;
+                    userService.userMess.realName = $scope.userInfo.realName;
+                } else {
                     $scope.alertTab(data.list.message);
                 }
             })
         }
 
         //调用原生api设置用户头像
-        $scope.setPortrait=function(){
-            var hideSheet=$ionicActionSheet.show({
-                buttons:[
-                    {text:'从相册选择'},
-                    {text:'从相机拍照'}
+        $scope.setPortrait = function () {
+            var hideSheet = $ionicActionSheet.show({
+                buttons: [
+                    {text: '从相册选择'},
+                    {text: '从相机拍照'}
                 ],
-                titleText:'选择相机或者相册',
-                cancelText:'取消',
-                buttonClicked:function(index){
-                    var data={type:index};
+                titleText: '选择相机或者相册',
+                cancelText: '取消',
+                buttonClicked: function (index) {
+                    var data = {type: index};
                     connectWebViewJavascriptBridge(function (bridge) {
                         //回app
                         bridge.callHandler('modifyAvatar', data, function (response) {
@@ -77,66 +77,66 @@ angular.module('swalk.userinfo', [])
         }
 
         //去修改用户姓名
-        $scope.toModifyName=function(){
-            $state.go('setUsername',{});
+        $scope.toModifyName = function () {
+            $state.go('setUsername', {});
             $ionicViewSwitcher.nextDirection('forward');
         }
 
         //去修改密码
-        $scope.toModifyPassowrd=function(){
-            $state.go('modifyPassowrd',{});
+        $scope.toModifyPassowrd = function () {
+            $state.go('modifyPassowrd', {});
             $ionicViewSwitcher.nextDirection('forward');
         }
     }])
-    .controller('userInfoSave',['$scope','$state','userService','$filter','$ionicActionSheet',function($scope,$state,userService,$filter,$ionicActionSheet){
-        $scope.userInfo={};
-        angular.copy($state.params.obj,$scope.userInfo);
+    .controller('userInfoSave', ['$scope', '$state', 'userService', '$filter', '$ionicActionSheet', function ($scope, $state, userService, $filter, $ionicActionSheet) {
+        $scope.userInfo = {};
+        angular.copy($state.params.obj, $scope.userInfo);
         /*var date=new Date(parseInt($scope.userInfo.birthday));
-        var year=date.getFullYear();
-        var month=(date.getMonth()>9)?date.getMonth()+1:'0'+(date.getMonth()+1);
-        var myDate=(date.getDate()>9)?date.getDate():'0'+date.getDate();
-        $scope.userInfo.birthday=year+'-'+month+'-'+myDate;*/
-        $scope.userInfo.birthday=new Date($scope.userInfo.birthday);
+         var year=date.getFullYear();
+         var month=(date.getMonth()>9)?date.getMonth()+1:'0'+(date.getMonth()+1);
+         var myDate=(date.getDate()>9)?date.getDate():'0'+date.getDate();
+         $scope.userInfo.birthday=year+'-'+month+'-'+myDate;*/
+        $scope.userInfo.birthday = new Date($scope.userInfo.birthday);
 
-        $scope.saveChangeInfo=function(){
+        $scope.saveChangeInfo = function () {
             updateUserInfo($scope.userInfo);
         }
-        function updateUserInfo(val){
-            var phoneReg=/^1[3|5|7|8]\d{9}$/;
-            if(!phoneReg.exec($scope.userInfo.phone)){
+        function updateUserInfo(val) {
+            var phoneReg = /^1[3|5|7|8]\d{9}$/;
+            if (!phoneReg.exec($scope.userInfo.phone)) {
                 $scope.alertTab('请输入正确的手机号码');
                 return;
             }
-            userService.updateUserInfo(val).then(function(data){
-                if(data.list.errcode==10000){
-                    $scope.alertTab('修改成功',$scope._goback(-1));
-                }else{
+            userService.updateUserInfo(val).then(function (data) {
+                if (data.list.errcode == 10000) {
+                    $scope.alertTab('修改成功', $scope._goback(-1));
+                } else {
                     $scope.alertTab(data.list.message);
                 }
             })
         }
 
-        $scope.change=function(){
+        $scope.change = function () {
 
         }
 
-        $scope.choiceGender=function(val){
-            if($scope.userInfo.gender==val){
+        $scope.choiceGender = function (val) {
+            if ($scope.userInfo.gender == val) {
                 return;
             }
-            $scope.userInfo.gender=val
+            $scope.userInfo.gender = val
         }
 
-        $scope.showSheet=function(){
-            var hideSheet=$ionicActionSheet.show({
-                buttons:[
-                    {text:'从相册选择'},
-                    {text:'从相机拍照'}
+        $scope.showSheet = function () {
+            var hideSheet = $ionicActionSheet.show({
+                buttons: [
+                    {text: '从相册选择'},
+                    {text: '从相机拍照'}
                 ],
-                titleText:'选择相机或者相册',
-                cancelText:'取消',
-                buttonClicked:function(index){
-                    var data={type:index};
+                titleText: '选择相机或者相册',
+                cancelText: '取消',
+                buttonClicked: function (index) {
+                    var data = {type: index};
                     connectWebViewJavascriptBridge(function (bridge) {
                         //回app
                         bridge.callHandler('modifyAvatar', data, function (response) {
@@ -148,27 +148,29 @@ angular.module('swalk.userinfo', [])
         }
         connectWebViewJavascriptBridge(function (bridge) {
             bridge.registerHandler('uploadAvatarSucceed', function (response) {
-                userService.userMess.portrait=response;
-                $scope.userInfo.portrait=response;
+                userService.userMess.portrait = response;
+                $scope.userInfo.portrait = response;
                 $scope.$digest();
             })
         });
     }])
-    .controller('setUserName',['$scope','$state','$ionicViewSwitcher',function($scope,$state,$ionicViewSwitcher){
-        $scope.userName='富康小龙';
-        $scope.sureSubmit=function(){
-            console.log(!$scope.userName);
-            if(!$scope.userName){
+    .controller('setUserName', ['$scope', '$state', '$ionicViewSwitcher', function ($scope, $state, $ionicViewSwitcher) {
+        $scope.user = {
+            name: '富康小龙'
+        };
+        $scope.sureSubmit = function () {
+            console.log(!$scope.user.name);
+            if (!$scope.user.name) {
                 $scope.alertTab('请输入正确的用户名');
             }
         };
 
-        $scope.cleanName=function(){
-            $scope.userName='';
+        $scope.cleanName = function () {
+            $scope.user.name = '';
         }
     }])
-    .controller('setUserPassword',['$scope','$state','$ionicViewSwitcher',function($scope,$state,$ionicViewSwitcher){
-        $scope.sureSubmit=function(){
+    .controller('setUserPassword', ['$scope', '$state', '$ionicViewSwitcher', function ($scope, $state, $ionicViewSwitcher) {
+        $scope.sureSubmit = function () {
             $scope.alertTab('修改成功');
         }
     }])
--- src/app/routes.js
+++ src/app/routes.js
@@ -18,65 +18,6 @@ angular.module('swalk.route', [])
                     }
                 }
             })
-            .state('tabs.recholiday', {
-                url: '/second/:id/:picid/:city',
-                views: {
-                    'home-tab': {
-                        templateUrl: 'app/modules/home/home.holiday.html',
-                        controller: 'recHoliday'
-                    }
-                }
-            })
-            .state('emplayvip', {
-                url: '/employee/vip',
-                templateUrl: 'app/modules/home/employee.html',
-                controller: 'emplayeeVip',
-                cache:'false'
-
-            })
-            .state('travelDetail', {
-                url: '/travel/detail/:pid',
-                templateUrl: 'app/modules/traveldetail/travel.html',
-                controller: 'travelDetail',
-                controllerAs:'travel'
-            })
-            .state('fillorder',{
-                params: {date: null, price: null,dateId:null,preOrderData:null},
-                url:'/travel/fillorder/:pid',
-                templateUrl:'app/modules/order/fill.order.html',
-                controller:'fillorder',
-                controllerAs:'order'
-            })
-            .state('choiceDate',{
-                params: {pid: null},
-                url:'/travel/fillorder/:pid/choiceDate',
-                templateUrl:'app/modules/order/fill.order.choiceDate.html',
-                controller: 'fillorderChoiceDateCtrl'
-            })
-            .state('payorder',{
-                url:'/pay/payorder',
-                templateUrl:'app/modules/pay/pay.html',
-                controller:'payctrl',
-                cache:'false'
-            })
-            .state('traveldate',{
-                url:'/travel/date',
-                templateUrl:'app/modules/traveldate/date.html',
-                controller:'travelDate',
-                controllerAs:'travelDate'
-            })
-            .state('choicecontact',{
-                url:'/travel/contact/:usefor',
-                templateUrl:'app/modules/contacts/contact.html',
-                controller:'choiceContact'
-            })
-
-            .state('addcontact',{
-                url:'/travel/add',
-                cache:'false',
-                templateUrl:'app/modules/addcontact/add.html',
-                controller:'addContact'
-            })
 
             .state('tabs.stay', {
                 params: {stayInDate: null, stayOutDate: null},
@@ -88,62 +29,7 @@ angular.module('swalk.route', [])
                     }
                 }
             })
-            .state('stayChoiceDate', {
-                params: {name: null},
-                url: "/stayChoiceDate",
-                templateUrl: "app/modules/stay/stay.choiceDate.html",
-                controller: 'stayChoiceDateCtrl',
-                cache:false
-            })
-            .state('citylist',{
-                url: '/list/city',
-                templateUrl: 'app/modules/city/city.html',
-                controller: 'cityList',
-                cache:false
-            })
-            .state('condition',{
-                url: '/device/choice',
-                templateUrl: 'app/modules/condition/term.html',
-                controller: 'serviceDevice',
-                cache:false
-            })
-            .state('staylist', {
-                params: {stayInDate: null, stayOutDate: null},
-                url: '/stay/list/:name',
-                templateUrl: 'app/modules/staylist/stay.list.html',
-                controller: 'stayList',
-                cache:false
-            })
-            .state('stayDetail', {
-                url: '/stay/detail/:pid',
-                templateUrl: 'app/modules/staydetail/stay.html',
-                controller: 'stayDetail'
-            })
-            //商品详情图片
-            .state('detailImage',{
-                params:{'data':''},
-                url: '/detail/images',
-                templateUrl: 'app/modules/imgs/images.html',
-                controller: 'detailImages',
-                cache:'false'
-            })
-            .state('stayOrder',{
-                params:{data:''},
-                url: '/stay/order/:pid',
-                templateUrl: 'app/modules/stayorder/order.html',
-                controller: 'stayFillorder'
-            })
-            .state('orderStayChoiceDate',{
-                params: {id: null},
-                url: '/stayOrderChoiceDate',
-                templateUrl: 'app/modules/stayorder/order.chioceDate.html',
-                controller: 'orderChioceDateCtrl'
-            })
-            .state('remark',{
-                url: '/stay/remark',
-                templateUrl: 'app/modules/remark/remark.html',
-                controller: 'stayRemark'
-            })
+
             .state('tabs.holiday', {
                 url: "/holiday",
                 views: {
@@ -154,7 +40,7 @@ angular.module('swalk.route', [])
                     }
                 }
             })
-
+            //我的界面
             .state('tabs.mine', {
                 url: "/mine",
                 cache:"false",
@@ -166,6 +52,7 @@ angular.module('swalk.route', [])
                     }
                 }
             })
+            //登录
             .state('login',{
                 params:{code:null},
                 url:'/login',
@@ -189,6 +76,7 @@ angular.module('swalk.route', [])
                 templateUrl:'app/modules/reg/reg.set.name.html',
                 controller:'regSetName'
             })
+            //忘记密码修改密码
             .state('newPassword',{
                 params:{phone:''},
                 url:'/password/reset',
@@ -202,76 +90,19 @@ angular.module('swalk.route', [])
                 controller:'userInfo',
                 cache:false
             })
+            //修改用户名
             .state('setUsername',{
                 url:'/modify/name',
                 templateUrl:'app/modules/user/set.name.html',
                 controller:'setUserName',
                 cache:false
             })
+            //修改登录密码
             .state('modifyPassowrd',{
                 url:'/modify/password',
                 templateUrl:'app/modules/user/set.password.html',
                 controller:'setUserPassword',
                 cache:false
-            })
-            .state('setuserinfo',{
-                params:{obj:''},
-                url:'/user/setting',
-                templateUrl:'app/modules/user/setinfo.html',
-                controller:'userInfoSave',
-                cache:false
-            })
-            //修改支付密码方式
-            .state('changetype',{
-                url:'/change/type',
-                templateUrl:'app/modules/change/type.html',
-                controller:'changeType',
-                cache:'false'
-            })
-            //修改登录密码方式
-            .state('loginpasswordtype',{
-                url:'/password/type',
-                templateUrl:'app/modules/password/type.html',
-                controller:'changeLoginType',
-                cache:'false'
-            })
-            //忘记支付密码
-            .state('identity',{
-                url:'/change/identity/:memery/:from',
-                templateUrl:'app/modules/identity/identity.html',
-                controller:'identity',
-                cache:'false'
-            })
-            .state('reset',{
-                url:'/change/reset/:memery',
-                templateUrl:'app/modules/reset/reset.html',
-                controller:'reset',
-                cache:'false'
-            })
-            .state('confirm',{
-                url:'/change/confirm/:memery',
-                templateUrl:'app/modules/confirm/confirm.html',
-                controller:'confirm',
-                cache:'false'
-            })
-            .state('agreement',{
-                url:'/swalk/agreement',
-                templateUrl:'app/modules/agreement/agreement.html',
-                cache:'false'
-            })
-            .state('goods',{
-                url:'/goods/orders/:from',
-                templateUrl:'app/modules/goods/goods.html',
-                controller:'myOrders',
-                controllerAs:'goods',
-                cache:'false'
-            })
-            //用户常用联系人信息
-            .state('comlinkman',{
-                url:'/mine/linkman',
-                templateUrl:'app/modules/comlink/linkman.html',
-                controller:'linkman',
-                cache:'false'
             })
             //设置
             .state('set',{
@@ -280,137 +111,12 @@ angular.module('swalk.route', [])
                 controller:'setting',
                 cache:'false'
             })
+            //关于我们
             .state('about',{
                 url:'/about/app',
                 templateUrl:'app/modules/about/about.html',
                 controller:'aboutApp'
             })
-            //我的收藏
-            .state('collection',{
-                url:'/mine/store',
-                templateUrl:'app/modules/store/store.html',
-                controller:'mineStore',
-                cache:'false'
-            })
-            //我的荣盛币
-            .state('rsb',{
-                params:{type:null},
-                url:'/rsb/myrsb',
-                templateUrl:'app/modules/rsb/rsb.html',
-                cache:'false',
-                controller:'myrsb'
-            })
-            //赠与输入手机号
-            .state('giftrsb',{
-                url:'/gift/rsb',
-                templateUrl:'app/modules/rsbgive/give.html',
-                controller:'giftrsb',
-                cache:false
-            })
-            //确认赠与荣盛币
-            .state('ensureGive',{
-                url:'/ensure/give',
-                templateUrl:'app/modules/rsbgive/sure.html',
-                controller:'suregive'
-            })
-            .state('giftresult',{
-                url:'/result/gift/:status',
-                templateUrl:'app/modules/rsbgive/result.html',
-                controller:'resultgive'
-            })
-            //优惠券
-            .state('coupon', {
-                url: "/coupon",
-                templateUrl: "app/modules/coupon/coupon.html",
-                controller: 'couponCtrl'
-            })
-            .state('couponDetail', {
-                url: "/coupon/:id",
-                templateUrl: "app/modules/coupon/coupon.detail.html",
-                controller: 'couponDetailCtrl'
-            })
-            //可用优惠券
-            .state('couponChoice',{
-                url:'/choice/coupon',
-                templateUrl:'app/modules/coupon/coupon.choice.html',
-                controller:'couponChoice'
-            })
-            //购买荣盛币
-            .state('buyrsb',{
-                url:'/buyrsb/buy',
-                templateUrl:'app/modules/buyrsb/buy.html',
-                cache:'false',
-                controller:'buyrsb'
-            })
-            //荣盛币使用说明
-            .state('explain',{
-                url:'/explain',
-                templateUrl:'app/modules/rsb/explain.html',
-                controller:'rsbexplain',
-                cache:'false'
-            })
-            //荣盛币购买说明
-            .state('buyexplain',{
-                url:'/buyrsb/explain',
-                templateUrl:'app/modules/buyrsb/explain.html'
-            })
-            //支付结果
-            //0-支付成功 1-未完全支付 2-未支付
-            .state('payresult',{
-                url:'/pay/result/:status/:type',
-                templateUrl:'app/modules/results/payment.html',
-                controller:'payment'
-            })
-            //我的资产
-            .state('asset',{
-                url:'/asset',
-                controller:'myAsset',
-                templateUrl:'app/modules/asset/asset.html'
-            })
-            .state('assetDetail',{
-                params:{"type": null},
-                url:'/asset/detail',
-                templateUrl:'app/modules/asset/asset.detail.html',
-                controller:'assetDetailCtrl'
-            })
-            .state('assetNews',{
-                url:'/asset/news',
-                templateUrl:'app/modules/asset/asset.news.html',
-                controller:'assetNewsCtrl'
-            })
-            .state('assetIncome',{
-                url:'/asset/income',
-                templateUrl:'app/modules/asset/asset.income.html',
-                controller:'assetIncomeCtrl'
-            })
-            .state('assetIncomeDetail',{
-                params:{income:null},
-                url:'/asset/income/detail',
-                templateUrl:'app/modules/asset/asset.income.detail.html',
-                controller:'assetIncomeDetail'
-            })
-            .state('assetHousekeeper',{
-                url: '/asset/housekeeper',
-                templateUrl: 'app/modules/asset/asset.housekeeper.html',
-                controller: 'housekeeperCtrl',
-                cache:false
-            })
-            .state('assetHousekeeperReservation',{
-                params:{"date": null,"appoint": false},
-                url: '/asset/housekeeper/reservation',
-                templateUrl: 'app/modules/asset/asset.housekeeper.reservation.html',
-                controller: 'housekeeperReservationCtrl'
-            })
-            .state('maintenanceRecord',{
-                url:'/asset/maintenance/record',
-                templateUrl:'app/modules/asset/asset.maintence.record.html',
-                controller:'maintenanceRecord'
-            })
-            .state('maintenanceRecordInfo',{
-                params:{date:null},
-                url:'/asset/repair/info',
-                templateUrl:'app/modules/asset/repairInfo.html',
-                controller:'maintenanceRecordInfo'
-            })
+
         $urlRouterProvider.otherwise("/tab/mine");
     });
