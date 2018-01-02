14a1d22039753a08109fbf9549a74b299659f1b0
jinbao <jinbaow@yeah.net>
2018-01-02 22:29:38 +0800

    我的收藏

--- src/app/modules/video/video.js
+++ src/app/modules/video/video.js
@@ -62,7 +62,7 @@ angular.module('tab.video', [])
                     url: urlStr + 'ym/show/list.api',
                     method: 'POST',
                     params: {
-                        rootId: 9
+                        rootId: 1
                     }
                 }).success(function (res) {
                     if (res.result == 1) {
@@ -82,7 +82,8 @@ angular.module('tab.video', [])
             }
             $scope.pageClick = function (val) {
                 console.log(val);
-                $scope.toAlbumDetail(val);
+                $scope.toAlbumDetail({id:val.itemId});
+
             }
 
 
@@ -94,8 +95,8 @@ angular.module('tab.video', [])
 
                     })
                 });
-                // $state.go('videoDetail', {detail:'list',rootId:1,id: val.id});
-                // $ionicViewSwitcher.nextDirection('forward');
+                $state.go('videoDetail', {detail:'list',rootId:1,id: val.id});
+                $ionicViewSwitcher.nextDirection('forward');
             }
 
             $scope.toCategory = function () {
